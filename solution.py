5ed879eeaef65c0e02ab9de4e75502522a5b8940
Chandu <chandu@b-10-27-85-185.dynapool.vpn.nyu.edu>
2020-10-10 16:40:42 -0400

    new STMP 2

--- solution.py
+++ solution.py
@@ -37,9 +37,11 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send DATA command and print server response.
     clientSocket.send(bytes(data.encode()))
+    recv1 = clientSocket.recv(1024).decode()
 
     # Send message data.
     clientSocket.send(bytes(msg.encode()))
+    recv1 = clientSocket.recv(1024).decode()
 
 
     # Message ends with a single period.
