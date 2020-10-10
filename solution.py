a3124bd414dc979ec1747540b189b99007fb77df
Chandu <chandu@b-10-27-85-185.dynapool.vpn.nyu.edu>
2020-10-10 16:45:51 -0400

    new STMP 4

--- solution.py
+++ solution.py
@@ -26,19 +26,34 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Send MAIL FROM command and print server response.
     mailFrom = "MAIL FROM:<sa6274@nyu.edu>\r\n"
     clientSocket.send(bytes(mailFrom.encode()))
+    recv2 = clientSocket.recv(1024).decode()
+    # print(recv2)
+    # if recv2[:3] != '250':
+    #   print('250 reply not received from server.')
 
     # Send RCPT TO command and print server response.
     rcptTo = "RCPT TO:<sa6274@nyu.edu>\r\n"
     clientSocket.send(bytes(rcptTo.encode()))
+    recv3 = clientSocket.recv(1024).decode()
+    # print(recv3)
+    # if recv3[:3] != '250':
+    #   print('250 reply not received from server.')
 
     # Send DATA command and print server response.
     data = 'DATA\r\n'
     clientSocket.send(bytes(data.encode()))
+    recv4 = clientSocket.recv(1024).decode()
+    # print(recv4)
+    # if recv4[:3] != '250':
+    #   print('250 reply not received from server.')
 
     # Send message data.
     msg = "\r\n My message"
     clientSocket.send(bytes(msg.encode()))
-
+    recv5 = clientSocket.recv(1024).decode()
+    # print(recv5)
+    # if recv5[:3] != '250':
+    #   print('250 reply not received from server.')
 
     # Message ends with a single period.
     endmsg = "\r\n.\r\n"
